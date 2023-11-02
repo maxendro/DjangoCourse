6a809a2ee6149cc5620c22f81e35571f65070142
Кирилл Кузнецов <maxendro@gmail.com>
2023-11-02 15:54:31 +0300

    Initial commit

---
+++ NewsSite/contacts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def contacts (request):
+    return HttpResponse("<h1>Contacts</h1>")
+    #return render(request, 'main/index.html')
+
